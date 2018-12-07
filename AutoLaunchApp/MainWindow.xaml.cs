6d6e3ccd9488df18e0a3c377b3bb94ca0280c300
Zheng_Xing <zheng_xing@dell.com>
2018-12-07 17:35:25 +0800

    add enable/disable startup task feature on UI

--- AutoLaunchApp/MainWindow.xaml.cs
+++ AutoLaunchApp/MainWindow.xaml.cs
@@ -1,28 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Windows.ApplicationModel;
+using Windows.UI.Popups;
 
 namespace AutoLaunchApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private bool isChecked;
+
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set
+            {
+                isChecked = value;
+                this.OnPropertyChanged();
+                UpdateStartupTaskState();
+            }
+        }
+
+        private StartupTaskState state;
+
+        public StartupTaskState State
+        {
+            get { return state; }
+            private set
+            {
+                state = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private async Task UpdateStartupTaskState()
+        {
+            var startupTask = await StartupTask.GetAsync("AutoLaunchAppTask");
+            State = startupTask.State;
+            if (IsChecked)
+            {
+                switch (State)
+                {
+                    case StartupTaskState.Disabled:
+                        // Task is disabled but can be enabled.
+                        State = await startupTask.RequestEnableAsync(); // ensure that you are on a UI thread when you call RequestEnableAsync()
+                        break;
+                    case StartupTaskState.DisabledByUser:
+                        // Task is disabled and user must enable it manually.
+                        MessageDialog dialog = new MessageDialog(
+                            "You have disabled this app's ability to run " +
+                            "as soon as you sign in, but if you change your mind, " +
+                            "you can enable this in the Startup tab in Task Manager.",
+                            "TestStartup");
+                        await dialog.ShowAsync();
+                        break;
+                    case StartupTaskState.DisabledByPolicy:
+                        Debug.WriteLine("Startup disabled by group policy, or not supported on this device");
+                        break;
+                }
+            }
+            else
+            {
+                startupTask.Disable();
+                State = startupTask.State;
+            }
         }
     }
 }
